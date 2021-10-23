97e6f2066661e4e763394b091677bdbdd2cb5a43
yusuke komatsu <lanos.komatsu@gmail.com>
2021-10-23 18:22:19 +0900

    add

--- ch4/maze2.py
+++ ch4/maze2.py
@@ -13,22 +13,23 @@ maze = [
     [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
 ]
 
-pos = [[1, 1, 0]]
-
-while len(pos) > 0:
-    x, y, depth = pos.pop(0)
-
+def search(x, y, depth):
     if maze[x][y] == 1:
         print(depth)
-        break
+        exit()
 
     maze[x][y] = 2
 
     if maze[x - 1][y] < 2:
-        pos.append([x - 1, y, depth + 1])
+        search(x - 1, y, depth + 1)
     if maze[x + 1][y] < 2:
-        pos.append([x + 1, y, depth + 1])
+        search(x + 1, y, depth + 1)
     if maze[x][y - 1] < 2:
-        pos.append([x, y - 1, depth + 1])
+        search(x, y - 1, depth + 1)
     if maze[x][y + 1] < 2:
-        pos.append([x, y + 1, depth + 1])
+        search(x, y + 1, depth + 1)
+
+
+    maze[x][y] = 0
+
+search(1, 1, 0)
